bf45665b0f38f2f76a040e2bd2ef2e9af9b5ae5a
alexanderdombroski <dom21013@byui.edu>
2024-11-13 11:01:41 -0700

    fix! udpated vite config for multiple pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,21 @@ import deno from '@deno/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src',
+  base: '/thanksgiving/',
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.webp', '**/*.avif'],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'src/index.html', 
+        gratitude: 'src/gratitude.html',
+        // quiz: 'src/quiz.html',
+        // recipes: 'src/gratitude.html'
+      }
+    }
+  },
   plugins: [deno()],
+  publicDir: 'public'
 })
