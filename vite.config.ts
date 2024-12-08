5a7db9a37ec541ae50f58f04d81e6e00efafe8be
alexanderdombroski <dom21013@byui.edu>
2024-12-07 20:03:46 -0700

    removed base and added recipes page for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import deno from '@deno/vite-plugin'
 // https://vite.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/thanksgiving/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.webp', '**/*.avif'],
   build: {
     outDir: '../dist',
@@ -14,7 +13,7 @@ export default defineConfig({
         main: 'src/index.html', 
         gratitude: 'src/gratitude.html',
         quiz: 'src/quiz.html',
-        // recipes: 'src/gratitude.html'
+        recipes: 'src/recipe-page.html'
       }
     }
   },
